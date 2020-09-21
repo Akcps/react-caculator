56eb0d9f8d91e3f852078d11d429dd8ccf48da2d
akcps <ajitkumarjsr15@gmail.com>
2020-09-21 21:27:48 +0530

    Updates

---
+++ precache-manifest.b4f96dff6dcb0aa40c02a2457c429d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65971fda90b07487ff337343d5b41fa2",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "954bdfa696b6f8433f3e",
+    "url": "/react-calculator/static/css/main.603da9cc.chunk.css"
+  },
+  {
+    "revision": "652ba1c2bb515c22ec15",
+    "url": "/react-calculator/static/js/2.ecff6520.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.ecff6520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954bdfa696b6f8433f3e",
+    "url": "/react-calculator/static/js/main.b8402a80.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  }
+]);
